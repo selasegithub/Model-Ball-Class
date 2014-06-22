ed268871e025a92cf4a589e9be91e821a69050f0
Richard Kwame Selase Addrah <selasegithub@users.noreply.github.com>
2014-06-22 15:03:44 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "PVector.h"
+
+using namespace std;
+
+int main()
+{
+	PVector pvec;
+	cout << pvec.GetX() << "\n";
+	cout << pvec.GetY() << "\n";
+
+	cin.get();
+	return 0;
+}
